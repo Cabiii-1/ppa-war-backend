14504e66f0dcf15c44deac8c8d2cb2aca1627496
Cj <christianjaylordc@gmail.com>
2025-09-18 17:18:38 +0800

    bday jek

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'PPA WAR API Server', 'status' => 'running']);
 });
